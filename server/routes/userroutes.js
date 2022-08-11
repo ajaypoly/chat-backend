dc931ee08a1e11e8cfc41d8ddc3d5f40c5b447cd
AJAY POLY <ajay@hamon.in>
2022-08-11 16:28:14 +0530

    complete login page

--- server/routes/userroutes.js
+++ server/routes/userroutes.js
@@ -1,7 +1,8 @@
-const { register } = require("../controllers/usercontroller")
+const { register, login } = require("../controllers/usercontroller")
 
 const router =require("express").Router()
 
 router.post("/register",register)
+router.post("/login",login)
 
 module.exports = router
